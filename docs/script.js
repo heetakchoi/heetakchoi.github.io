3a2d76e1e90afe08b5254a92091e46cc7bb1d854
Heetak Choi <endofhope@naver.com>
2022-06-26 13:26:26 +0900

    블로그 추가

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,8 @@
 function move_to_latest_article(){
-    document.location = "articles/article-187.html";
+    document.location = "articles/article-188.html";
 }
 function move_to_latest_article_indepth(){
-    document.location = "../articles/article-187.html";
+    document.location = "../articles/article-188.html";
 }
 function move_to_latest_book(){
     document.location = "books/book-1.html";
