23a8f65eb94f428287802d9130612f96ca2e701f
Heetak Choi <endofhope@naver.com>
2026-09-04 23:37:46 +0900

    책 소개 추가

--- docs/script.js
+++ docs/script.js
@@ -5,8 +5,8 @@ function move_to_latest_article_indepth(){
     document.location = "../articles/article-93.html";
 }
 function move_to_latest_book(){
-    document.location = "books/book-2.html";
+    document.location = "books/book-3.html";
 }
 function move_to_latest_book_indepth(){
-    document.location = "../books/book-2.html";
+    document.location = "../books/book-3.html";
 }
